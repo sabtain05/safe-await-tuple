30d59b33c82f4a9b04c151f4c1f468db7ed97299
Sabtain Ali <sabtainalipk144@gmail.com>
2026-09-04 12:43:34 +0500

    Add documentation for promise wrapper function

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Wraps a promise to return a Go-style tuple: [error, result].
+ * If the promise resolves successfully, the error is null.
+ * If the promise rejects, the result is null and the error is guaranteed to be an Error object.
+ *
+ * @param promise The asynchronous operation to wrap.
+ * @returns A promise that resolves to a tuple of [Error, null] or [null, Data].
+ */
