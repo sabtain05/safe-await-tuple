4a4f4ae289493576e712a6457ebacef00f6f1d73
Sabtain Ali <sabtainalipk144@gmail.com>
2026-09-05 10:52:38 +0500

    Fix import path for safe and safeSync in main-test.js

--- Test/main-test.js
+++ Test/main-test.js
@@ -1,4 +1,4 @@
-const { safe, safeSync } = require('./dist/index.js');
+const { safe, safeSync } = require('../dist/index.js');
 
 async function testV11() {
     console.log("Testing safe-await-tuple v1.1.0 (Sync & Async)...\n");
