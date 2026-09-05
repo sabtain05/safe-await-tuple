7c7b40c5a5497606c4245a0952ef6745e9eb4a3d
Sabtain Ali <sabtainalipk144@gmail.com>
2026-09-05 23:45:33 +0500

    feat: implement logging in testV15 function for safe-await-tuple v1.5.0

--- Test/main-test.js
+++ Test/main-test.js
@@ -1,3 +1,5 @@
 const { safe, safeSync, safeAll } = require('../dist/index.js');
 
-async function testV15() {}
+async function testV15() {
+    console.log("Testing safe-await-tuple v1.5.0...\n");
+}
