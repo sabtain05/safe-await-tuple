1200908d80e05652f9959df5246cdb9b14c1c112
Sabtain Ali <sabtainalipk144@gmail.com>
2026-09-05 10:37:45 +0500

    Add safeSync call in testV11 function to parse JSON data

--- Test/main-test.js
+++ Test/main-test.js
@@ -2,4 +2,6 @@ const { safe, safeSync } = require('./dist/index.js');
 
 async function testV11() {
     console.log("Testing safe-await-tuple v1.1.0 (Sync & Async)...\n");
+
+    const [err1, data1] = safeSync(()=> JSON.parse('{"name": "Sabtain"}'));
 }
