82d376e91805eb88b23c7635e4d15750dc625591
Sabtain Ali <sabtainalipk144@gmail.com>
2026-09-05 23:46:43 +0500

    feat: add promise resolution and rejection in testV15 function

--- Test/main-test.js
+++ Test/main-test.js
@@ -2,4 +2,7 @@ const { safe, safeSync, safeAll } = require('../dist/index.js');
 
 async function testV15() {
     console.log("Testing safe-await-tuple v1.5.0...\n");
+
+    const p1 = Promise.resolve("Data 1");
+    const p2 = Promise.reject(new Error("Database timeout"));
 }
