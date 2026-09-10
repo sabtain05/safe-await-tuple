ad0e56637e2013a139c054d5a68f35116d186a98
Sabtain Ali <sabtainalipk144@gmail.com>
2026-09-10 22:08:13 +0500

    refactor: remove redundant comments in main-test.js for clarity

--- Test/main-test.js
+++ Test/main-test.js
@@ -1,6 +1,6 @@
 const { safe } = require('../dist/index.js');
 
-// 1. Define a custom error class with extra properties
+
 class CustomHttpError extends Error {
   constructor(message, statusCode) {
     super(message);
@@ -12,7 +12,7 @@ class CustomHttpError extends Error {
 async function testV2() {
   console.log("Testing safe-await-tuple v2.0.0 (Custom Errors)...\n");
 
-  // 2. Reject a promise using our custom error
+  
   const failingApiCall = Promise.reject(new CustomHttpError("Resource Not Found", 404));
   
   const [err, data] = await safe(failingApiCall);
